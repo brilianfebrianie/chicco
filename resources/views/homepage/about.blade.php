ea1edb1d8abf165d7443192abdea307c9f65e73e
Brilian Febrianie <brilianfebrianie8@gmail.com>
2022-07-04 17:14:15 +0700

    Finished

---
+++ resources/views/homepage/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.template')
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1>Tentang Kami</h1>
+      <p>Chicco Swalayan adalah toko swalayan online berbasis website yang berdiri sejak tahun 2022. Chicco Swalayan menjual berbagai produk yang Anda butuhkan dengan harga terjangkau. Berbagai produk yang higenis dan berkualitas siap untuk melengkapi kebutuhan harian Anda!</p>
+    </div>
+  </div>
+</div>
+@endsection
---
+++ resources/views/homepage/kontak.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.template')
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1>Kontak Kami</h1>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Brilian Febrianie</h5>
+                <p class="card-text">083854120339</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
